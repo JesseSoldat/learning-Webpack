6d085b88e8e26eff37e02a8023e4895c2c4480ad
jesse soldat <jsoldat@hotmail.com>
2016-06-02 21:47:15 +0700

    react and es6 is now working in webpack

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,7 +1,22 @@
-var config = require('./config.json');
+import React from 'react'
+import config from './config.json';
 
-module.exports = function() {
-  var greet = document.createElement('div');
-  greet.textContent = config.greetText;
-  return greet;
-};
+export default React.createClass({
+	render: function() {
+		return(
+			<div>
+				<h2>React is now working in Webpack</h2>
+				<h4>{config.greetText}</h4>
+			</div>
+			);
+	}
+});
+
+
+// let config = require('./config.json');
+
+// module.exports = function() {
+//   let greet = document.createElement('div');
+//   greet.textContent = config.greetText;
+//   return greet;
+// };
--- app/main.js
+++ app/main.js
@@ -1,2 +1,10 @@
-var greeter = require('./Greeter.js');
-document.getElementById('root').appendChild(greeter());
+import React from 'react';
+import ReactDom from 'react-dom';
+import Greeter from './Greeter';
+
+ReactDom.render(<Greeter />, document.getElementById('root'));
+
+
+
+// let greeter = require('./Greeter.js');
+// document.getElementById('root').appendChild(greeter());
